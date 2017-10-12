533a42a208d3c5d0aef8f90e68b82fe22f2627b3
Abel Soto <abel.soto@bo.truextend.com>
2017-10-12 16:05:35 -0400

    update the code

--- sql/demo/CreateTables.sql
+++ sql/demo/CreateTables.sql
@@ -5,7 +5,7 @@ CREATE TABLE BANK.TR_ACCOUNT
   END_DATE        DATE,
   STATUS          VARCHAR2(1)
   )
-TABLESPACE TBS_BANK_DAT ;
+TABLESPACE TBS_BANK_DATA ;
 
 alter table BANK.TR_ACCOUNT
   add constraint PK_ACCOUNT_ID primary key (ACCOUNT_ID)
@@ -15,7 +15,8 @@ alter table BANK.TR_ACCOUNT
 CREATE TABLE BANK.TR_DAILY
 (
   DAILY_ID   		 NUMBER(15)  NOT NULL,
-  ACCOUNT_ID 		 NUMBER(15)  NOT NULL,
+  ACCOUNT_ID_ORIGIN 		 NUMBER(15)  NOT NULL,
+  ACCOUNT_ID_DESTIN 		 NUMBER(15)  NOT NULL,
   AMOUNT	 		 NUMBER(15,2)     ,	
   TYPE_TRANSACTION 	 VARCHAR2(50),   --Tipo T= Traspaso, D debito  H haber
   START_TRAN_DATE    DATE,
@@ -24,13 +25,22 @@ CREATE TABLE BANK.TR_DAILY
   END_USER_CODE      VARCHAR2(50),
   TRAN_ID            NUMBER(15)
 )
-TABLESPACE TBS_BANK_DAT;
+TABLESPACE TBS_BANK_DATA;
 alter table BANK.TR_DAILY
   add constraint PK_DAILY_ID primary key (DAILY_ID)
   using index 
   tablespace TBS_BANK_IDX;
 
-  
+/*alter table  BANK.TR_DAILY
+add constraint FK_ACCOUN_ID_ORIGIN FOREIGN KEY (ACCOUNT_ID_ORIGIN)
+references BANK.TR_ACCOUNT (ACCOUNT_ID)
+initially deferred deferrable;
+
+alter table  BANK.TR_DAILY
+add constraint FK_ACCOUN_ID_DESTIN FOREIGN KEY (ACCOUNT_ID_DESTIN)
+references BANK.TR_ACCOUNT (ACCOUNT_ID)
+initially deferred deferrable;*/
+
   --sequences
   
 create sequence BANK.SEQ_ACCOUNT
--- sql/demo/createTablespace.sql
+++ sql/demo/createTablespace.sql
@@ -1,13 +1,18 @@
+--DROP TABLESPACE TBS_BANCO_DATA    INCLUDING CONTENTS AND DATAFILES;
+select * from dba_tablespaces;
+select * from V$TABLESPACE;
+select * from ALL_DIRECTORIES;
+select * from DBA_DATA_FILES;
 select * from dba_users;
 
 create tablespace 
-   TBS_BANCO_DATA
+   TBS_BANK_DATA
 datafile   
   '/u01/app/oracle/oradata/oracle11g-data/TBS_BANK_DATA.dbf' 
 size 50m
 autoextend on;
 create tablespace 
-   TBS_BANCO_IDX
+   TBS_BANK_IDX
 datafile   
   '/u01/app/oracle/oradata/oracle11g-data/TBS_BANK_IDX.dbf' 
 size 50m
