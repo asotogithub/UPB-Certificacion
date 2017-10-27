daba246e0799af6f45ad269d236813d238edde6e
Abel Soto <abel@abelmacbook.local>
2017-10-27 19:57:15 -0400

    global variables

---
+++ sql/demo/global_variables.sql
@@ -0,0 +1,14 @@
+create or replace PACKAGE GLOBAL_VARIABLE AS
+    k_status_active CONSTANT TR_ACCOUNT.STATUS%TYPE := 'A';
+    k_status_inactive CONSTANT TR_ACCOUNT.STATUS%TYPE := 'I';
+    k_end_date        CONSTANT TR_ACCOUNT.END_DATE%TYPE := TO_DATE('31-12-9999','DD-MM-YYYY');
+
+    k_tran_type_deposit CONSTANT TR_DAILY.TYPE_TRANSACTION%TYPE := 'DEPOSITO';
+    k_tran_type_tranfer CONSTANT TR_DAILY.TYPE_TRANSACTION%TYPE := 'TRANSFERENCIA';
+    k_tran_type_withdraw CONSTANT TR_DAILY.TYPE_TRANSACTION%TYPE := 'RETIRO';
+    k_tran_type_init     CONSTANT TR_DAILY.TYPE_TRANSACTION%TYPE := 'APERTURA';
+
+
+    g_param_in VARCHAR2(3000);
+
+END;
